2cd845d0ba69418c33b8f843a37e97231a393b1d
Alfred Boateng <programmingwithalfred@gmail.com>
2018-09-19 07:44:09 +0000

    Welcome

--- canteen/src/Components/Home.js
+++ canteen/src/Components/Home.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import '../App.css';
 import Slider from './Slider'
+import Welcome from './Welcome'
 
 class Home extends Component {
   render() {
     return (
       <div>
        <Slider />
+       <Welcome  />
       </div>
     );
   }
--- canteen/src/Components/Welcome.js
+++ canteen/src/Components/Welcome.js
@@ -1,1 +1,25 @@
 import React from 'react';
+
+const Welcome =() =>(
+    <div className="container-fluid padding">
+       <div className="row welcome text-center" >
+            <div className="col-12">
+                <h1 className="display-4" >The ultimate canteen</h1>
+            </div>
+            <hr />
+            <div className="col-12">
+                <p className="lead">
+                    Nadia canteen is ready to give you the best food from all parts of the world whisch is so delicious and aye catching you cant pass by!
+                </p>
+            </div>
+            <div className="row padding text-center">
+                <div className="col-xs-12 col-sm-6 col-md-4" >
+
+                </div>
+            </div>
+            <hr className="my-4" />
+       </div>
+
+    </div>
+)
+export default Welcome;
